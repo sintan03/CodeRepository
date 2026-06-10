808562caf2b766ee479d60a2897a0691dd1cff0c
SineTangent03 <sin24steel@gmail.com>
2026-06-10 21:08:15 +0900

    Vector2.java の作成

---
+++ Java/Maze/Vector2.java
@@ -0,0 +1,20 @@
+package aabout.Java.Maze;
+
+public class Vector2 {
+    
+    private double x = 0;
+    private double y = 0;
+    
+    public Vector2(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Vector2(double x) { Vector2(x, 0); }
+    public Vector2() { Vector2(0, 0); }
+    
+    public String get() {
+        return String.valueOf(x) + "," + String.valueOf(y)
+    }
+    
+}
