f59c49a56ab643891a197e05b5f2ea021dbb217e
SineTangent03 <sin24steel@gmail.com>
2026-06-10 21:07:23 +0900

    Main.java の作成

---
+++ Java/Maze/Main.java
@@ -0,0 +1,26 @@
+package aabout.Java.Maze;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        MazeData mazeData;
+        Utils.out("New Game?(y/n): ");
+        String str = scan.next();
+        
+        if (str.equals("y")) {
+            Utils.out(Utils.randomInt(100));
+        } else {
+            do {
+                Utils.out("Save Data?(\"q\" is quit): ");
+                str = scan.next();
+            } while (!(str.equals("q") || MazeUtils.testSave(str)));
+            Utils.out("Loaded!");
+        }
+        
+    }
+    
+}
